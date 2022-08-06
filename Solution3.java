938ff5e13ddcc596d77e96b60ff476ac60b1f310
Sabari <r.sabarinathan02@gmail.com>
2022-08-06 12:16:09 +0530

    Create Solution3.java

---
+++ Solution3.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+class Result {
+
+    /*
+     * Complete the 'fibonacciModified' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER t1
+     *  2. INTEGER t2
+     *  3. INTEGER n
+     */
+
+    public static long fibonacciModified(int te1, int te2, int n) {
+    // Write your code here
+    long t3=0,temp=0;
+    long t1=te1,t2=te2;
+    for(int i=2;i<n;i++){
+        t3=t1+(t2*t2);
+        System.out.println(t3);
+        temp=t2;
+        t2=t3;
+        t1=temp;
+        
+        
+    }
+    return t3;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int t1 = Integer.parseInt(firstMultipleInput[0]);
+
+        int t2 = Integer.parseInt(firstMultipleInput[1]);
+
+        int n = Integer.parseInt(firstMultipleInput[2]);
+
+        long result = Result.fibonacciModified(t1, t2, n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
