1cb04d8dac1ef1628625488b2c7d96636884ac78
Sabari <r.sabarinathan02@gmail.com>
2023-01-14 17:04:52 +0530

    charPrint

---
+++ CharPrint.java
@@ -0,0 +1,14 @@
+/**
+ * CharPrint
+ */
+public class CharPrint {
+
+   public static void main(String[] args) {
+       char A = 'A';
+         char a = 'a';
+         
+         System.out.println((char)(a-A));
+         System.out.println((int) (a - A));
+            System.out.println((int) (A - a));
+   }
+}
